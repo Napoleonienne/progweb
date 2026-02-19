4c163700abf4d5bdc178f754f01cc731fd51375d
Napoléon <eliassaint.matthias@gmail.com>
2026-02-20 00:09:18 +0100

    jk

---
+++ tp4/1.js
@@ -0,0 +1,49 @@
+const triangleRectangle = function(n) {
+    for(let i =0 ; i<n; i++ ) {
+        for(let j =0;j<=i;j++){
+        print("*")
+
+        }
+    }
+    // Écrire le corps de la fonction ici.
+};
+// Écrire les tests ici.
+triangleRectangle(5)
+/* Question 2 */
+const triangleIsocèle = function(n) {
+    for(let i =0 ; i<n; i++ ) {
+    for(let j =0;j<=i;j++){
+        print("*")
+
+    }
+    }
+
+};
+// Écrire les tests ici.
+
+/* Question 3 */
+const rectangle = function(a,b) {
+    for(let i =0 ; i<a; i++ ) {
+    for(let j =0;j<=b;j++){
+        print("*")
+
+    }}
+
+
+    // Écrire le corps de la fonction ici.
+};
+// Écrire les tests ici.
+rectangle(10,5)
+/* Question 4 */
+const carré = function(n) {
+    // Écrire le corps de la fonction ici.
+};
+// Écrire les tests ici.
+
+/* Question 6 */
+noMoreConsole(); // Ne **surtout** pas effacer cette ligne
+const rectangle2 = function(a,b) {
+
+
+
+};
