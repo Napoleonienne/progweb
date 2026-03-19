6c4f9643af7a0583981d76b5079432c2e6b09aaf
Napoléon <eliassaint.matthias@gmail.com>
2026-03-19 22:57:33 +0100

    fait les questiot jusqu'a 6

--- tp6/memory.js
+++ tp6/memory.js
@@ -1,10 +1,50 @@
 const game = {};
 game.create = function(images, blank) {
+  this.images = images;
+  this.blank = blank;
+
+  this.cards = shuffleCards(this.images.length);
   
 };
+
+
+
+
+
 game.build = function(div) {
   
+    div.innerHTML = `${this.images.length}    <br>`;
+
+    for (let index_16 = 0; index_16 < this.cards.length; index_16++) {
+      let index_8=this.cards[index_16] ;
+          const img = document.createElement('img');
+          img.setAttribute('src', this.images[index_8]);
+          img.setAttribute('alt', "carte");
+          img.width = '80';
+          img.height = '80';
+          div.appendChild(img);
+          function click(){
+            let i = index_8;
+            let j = index_16;
+            let img2 = img
+            img.setAttribute('src',this.blank)
+            console.log(i,j)
+
+            
+            return 0;
+          }
+     
+          img.onclick = click;
+
+
+
+
+      
+    }
+
+  
 };
+random = n => Math.floor(n * Math.random())
 
 const shuffleCards = function(length) {
   let cards = [];
@@ -12,5 +52,7 @@ const shuffleCards = function(length) {
     cards.push(i);
     cards.push(i);
   }
-  return cards;
+  
+
+  return cards.sort(()=> Math.random());
 };
