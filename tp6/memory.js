2a1ef1a118aaec8fa76c1fc9591778b245c9c203
Napoléon <eliassaint.matthias@gmail.com>
2026-03-24 23:35:35 +0100

    Enhance memory game functionality and styling; improve card handling, click events, and visual effects

--- tp6/memory.js
+++ tp6/memory.js
@@ -5,108 +5,20 @@ game.create = function(images, blank) {
 
   this.cards = shuffleCards(this.images.length);
   this.carte_reveler=[]
-  this.compteur =
+  this.compteur =this.images.length;
 
 
 
-  //version 2
-  this.tb_carte =[].fill(0,0,7);
-
-
-  
-};
-var carte_reveler = []
-
-
-
-class carte {
-  constructor(dos_carte, carte) {
-    this.dos_carte = dos_carte;
-    this.carte = carte;
-    this.visible = false;
-    
-    this.img = document.createElement('img');
-    this.img.setAttribute('src', this.dos_carte);
-    this.img.setAttribute('alt', 'dos de la carte');
-    this.img.width = 80; 
-    this.img.height = 80;
-    
-    this.img.onclick = this.click.bind(this);
-  }
-
-  afficher() {
-    if (!this.visible) {
-      this.img.setAttribute('src', this.carte);
-      this.changement_etat();
-    }
-  }
-
-  cacher() {
-    if (this.visible) {
-      this.img.setAttribute('src', this.dos_carte);
-      this.changement_etat();
-    }
-  }
-  
-  changement_etat() {
-    this.visible = !this.visible;
-  }
-
-  click() {
-    if (!this.visible) {
-      this.afficher();
-    }
-  }
-}
-
-game.build2 = function(div) {
-
-  let div_p = document.createElement("p");
-  div_p.id ="paire";
-  div_p.innerText =` paire restanste : ${this.images.length}`
-
-  div.appendChild(div_p);
-  
-
-  
-  let bl = this.dos_carte;
-  
-  this.tb_carte = []; 
-
-  for (let index_16 = 0; index_16 < this.cards.length; index_16++) {
-    let index_8 = this.cards[index_16];
-    let ji = this.images[index_8];
-    
-    let nouvelle_carte = new carte(bl, ji);
-    
-    this.tb_carte[index_16] = nouvelle_carte;
-    
-    let div_img = document.createElement("div");
-    div_img.className
-    div_img.className = "carte";
-
-
-    div.appendChild(div_img);
-    
-    div_img.appendChild(nouvelle_carte.img);
-  }
 };
 
-game.logic2= function(){
-  this.tb_carte;
 
-  for (let index = 0; index < array.length; index++) {
-    const element = array[index];
-    
-  }
 
-}
 
 game.build = function(div) {
 
   
       let div_p = document.createElement("p");
-      div_p.id ="paire";
+      div_p.id ="paires";
       div_p.innerText =` paire restanste : ${this.compteur}`
 
       div.appendChild(div_p);
@@ -114,38 +26,63 @@ game.build = function(div) {
     for (let index_16 = 0; index_16 < this.cards.length; index_16++) {
       let index_8=this.cards[index_16] ;
       let ji = this.images[index_8];
-      let div_img =document.createElement("div")
-      div_img.className = "carte"
+      let div_img =document.createElement("div");
+      let div_fr =document.createElement("div");
+      let div_bc =document.createElement("div");
+
+
+      
+
+      div_img.className = "carteinner";
+
+
           const img = document.createElement('img');
           img.setAttribute('src', this.dos_carte);
           img.setAttribute('alt', "carte");
-          img.width = '80';
-          img.height = '80';
-          
-          div.appendChild(div_img);
-          div_img.appendChild(img);
+          div_fr.classList.add("carte_dessus");
+          div_fr.appendChild(img)
+
+
+          const img2 = document.createElement("img");
+          img2.setAttribute("src",this.images[index_8])
+          img2.setAttribute("alt","carte");
+          div_bc.classList.add("carte_arriere");
+          div_bc.appendChild(img2)
+           div_img.appendChild(div_bc)
+          div_img.appendChild(div_fr);
+            div.appendChild(div_img);
+
+
+
           
           function click(){
-                console.time();
+            let div_img1 = div_img;
+            div_img1.classList.add("carte_retourner") 
+            /*
+            if(this.carte_reveler.length < 2 && div_img1.id !="reveler"){
 
-            let i = index_8;
-            let j = index_16;
-            let img2 = img;
-            img2.setAttribute('src',this.images[i]);
+              let i = index_8;
+              let j = index_16;
+              let img2 = img;
+              
+              img2.setAttribute('src',this.images[i]);
+
+              
+              this.carte_reveler.push([j,i]);
+              this.logic(div);
+            }
+            */
+
+
             
-            this.carte_reveler.push(i);
 
 
-            
-
-
-            console.info(i,j);
 
             
             return 0;
           }
      
-          img.onclick = click.bind(this);
+          div_img.onclick = click.bind(this);
 
 
 
@@ -156,22 +93,63 @@ game.build = function(div) {
 };
 
 game.logic = function(div){
-        
+
+
+
+        if (this.compteur == 0) {
+          
+          
+        }
+ 
         if(this.carte_reveler.length ==2 ){
-            if(this.carte_reveler[0] == this.carte_reveler[1]){
-              let beta = div.children;
-              this.compteur -= 1;
-              div.querySelector("paires").innerText= ` paire restanste : ${this.compteur}`;
-            for (let i = 0; i < beta.length; i++) {
-              console.log(beta[i].textContent);
+          
+            let enfant = div.children
 
-            }}
-            else{
+            if(this.carte_reveler[0][1] == this.carte_reveler[1][1]){
+              this.compteur = this.compteur -1;
+              div.querySelector("#paires").innerText= ` paire restanste : ${this.compteur}`;
+              enfant[this.carte_reveler[0][0]+1].id ="reveler";
+              enfant[this.carte_reveler[1][0]+1].id ="reveler";
 
-            }
+             
+              
+
+
+
+      
+
+          this.carte_reveler = [];
+          }
+          for (let index =1; index<17;index++) {
+            let div_img = enfant[index];
+            let img =div_img.children[0];
+
+              
+                if( div_img.id !="reveler" ){
+                window.setTimeout(function(){                 
+                img.setAttribute("src",this.dos_carte);}.bind(this)
+                  ,500);
+                
+                
+              }}
+              this.carte_reveler = [];
+
+        if (this.compteur == 0) {
+            
+           if (window.confirm("vous avez gagner veuillez recharger la page")){
+             window.confirm("sometext");
+           };
+            
+
+         
+
+          
+          
+        }
 
 
         }
+
         
 
 
@@ -183,6 +161,7 @@ random = n => Math.floor(n * Math.random());
 const shuffleCards = function(length) {
   let cards = [];
   for(let i = 0; i < length; i++) {
+    
   
     cards.push(i);
     cards.push(i);
