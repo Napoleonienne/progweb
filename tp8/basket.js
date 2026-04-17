230310f57859389eee265f7606c0dfebb56f2f05
napoléon <eliassaint.matthias@gmail.com>
2026-04-17 12:36:42 +0200

    h

--- tp8/basket.js
+++ tp8/basket.js
@@ -5,11 +5,23 @@ ajax.onreadystatechange = function() {
   console.log("response : ",JSON.parse(
 ajax.responseText));
 };
-ajax.open("GET","https://vincentjuge1987.github.io/td/src/td08/fruitQuantities.json",true)
+ajax.open("GET","https://vincentjuge1987.github.io/td/src/td08/fruitQuantities.json",true);
 ajax.send();
 
-getd
 
+let b = JSON.parse(
+ajax.responseText);
+
+const j = document.getElementById("basket");
+const header = document.createElement("tr");
+j.appendChild(header)
+for (let index = 0; index < 4; index++) {
+    const col = document.createElement("th");
+    col.textContent = `{b}`
+    j.appendChild(col)
+
+    
+}
 
 
 
